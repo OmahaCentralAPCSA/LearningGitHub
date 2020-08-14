aa0d36479a0dc6b05bcf29c8ca7b3d5ab882a92b
Collin Holmquist <collinholmquist@gmail.com>
2020-08-14 15:20:50 -0500

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+class Main {
+
+  public static void main (String[] args) {
+  
+    System.out.println("Welcome to APCSA for the 2020 - 2021 School Year!");
+    //enter your statements below
+    
+    
+    for(int i = 0; i <= 10; i++) {
+      System.out.println("Omaha Central"); 
+      
+    }
+  }
+
+}
